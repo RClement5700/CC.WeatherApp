3c7ee0e8c15d0bc1852b0aaf39b39ea71e3f0664
rclement5700 <rclement5700>
2022-07-26 08:38:39 -0700

    Create Composables to display data from viewModel

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherCard.kt
@@ -0,0 +1,67 @@
+package com.plcoding.weatherapp.presentation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor: Color,
+    modifier: Modifier
+    ) {
+    state.weatherInfo?.currentWeatherData?.let { data ->
+        Card(
+            modifier = modifier.padding(16.dp),
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(10.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today ${
+                        data.time.format(
+                            DateTimeFormatter.ofPattern("HH:mm")
+                        )
+                    }",
+                    modifier = Modifier.align(Alignment.End),
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Image(
+                    modifier = Modifier.width(200.dp),
+                    painter = painterResource(id = data.weatherType.iconRes),
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "${data.temperatureCelsius}°C",
+                    fontSize = 50.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "${data.weatherType.weatherDesc}°C",
+                    fontSize = 20.sp,
+                    color = Color.White
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                WeatherDataDisplay()
+            }
+        }
+    }
+}
